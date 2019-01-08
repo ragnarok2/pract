75fa5f92a26e4466f08ab7902aa8172be2fd8dbc
ragnarok2 <rusvelt.teo2018@gmail.com>
2019-01-08 17:49:03 +0200

    add function validate to file validate.js

--- app/services/validate.js
+++ app/services/validate.js
@@ -4,3 +4,33 @@ export const password = /^([a-zA-Z0-9])+$/
 export const numberPhone =  /^\+?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{2})[-. ]?([0-9]{2})[-. ]?$/
 export const reglogin = /^([a-zA-Z0-9]{5-10})+$/
 export const regpassword = /^([a-zA-Z0-9]{6-10})+$/
+
+export const validate = (text, type) =>
+    {
+        login
+        password
+
+        if (type == 'login')
+        {
+            if (login.test(text)){
+                alert("login is validated")
+                console.log("login is validated")
+                this.setState({ login : text })
+            } else {
+                console.log("login isn`t validated")
+                alert("login isn`t validated")
+            }
+        }
+
+        if (type == 'password')
+        {
+            if (password.test(text)){
+                alert("password is validated")
+                console.log("password is validated")
+                this.setState({ password : text })
+            } else {
+                console.log("password isn`t validated")
+                alert("password isn`t validated")
+            }
+        }
+    }
